05f23d8b5c41c0f22ad27261cfdc52179e35bcac
kadiraj439 <krishadiraj@gmail.com>
2025-12-14 23:25:52 -0800

    Create Task.h

---
+++ PartA/Task.h
@@ -0,0 +1,22 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+
+class Task {
+private:
+    int id;
+    std::string description;
+    bool completed;
+
+public:
+    Task();
+    Task(int id, const std::string& desc);
+
+    void markCompleted();
+    bool isCompleted() const;
+    int getId() const;
+    std::string getDescription() const;
+};
+
+#endif
